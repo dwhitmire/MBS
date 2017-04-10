ce56f8d1801010d54827399c6e42006ee95fd666
dwhitmire <drewwhitmire@gmail.com>
2017-04-09 22:32:32 -0500

    api_search update
    api_search is now completely independent of the frontend that I was
    testing it with.  There were a couple components I forgot to decouple.
    
    I also know how to pass data between the front and back ends pretty well
    now, which is always a plus.

--- electron-quick-start/backend/api_search.js
+++ electron-quick-start/backend/api_search.js
@@ -1,43 +1,24 @@
 window.$ = window.jQuery = require('jquery')
-var movieAttributes = null
 
-$('#go').click(function() { // clicking "Go" button
-    var event = new Object()
-    event['keyCode'] = 13
-    searchAPI(event, addToArray)    
-}) 
+function searchAPI(title, year, callBack) {	
+    var url = 'http://www.omdbapi.com/?t=' + title + '&y=' + year + '&plot=short&r=json'
+    console.log(url)
+    console.log(title)
 
-
-function searchAPI(event, callBack) {	
-    if (event.keyCode == 13){ // if key press is enter 
-        
-        var title = $('#searchTitle').val()
-        var year = $('#searchYear').val()
-        var url = 'http://www.omdbapi.com/?t=' + title + '&y=' + year + '&plot=short&r=json'
-        console.log(url)
-        console.log(title)
-        
-        $.getJSON(url, function(movie) {
-            if (callBack) {					
-                callBack( {
-                    'title':movie.Title,
-                    'year':movie.Year,
-                    'rated':movie.Rated,
-                    'genre':movie.Genre,
-                    'director':movie.Director,
-                    'actors':movie.Actors,
-                    'plot':movie.Plot,
-                    'poster':movie.Poster
-                })
-            }            
-        })        
-    }
-}
-
-function addToArray(movieData) {
-    movieAttributes = movieData
-    console.log(movieAttributes['title'])
+    $.getJSON(url, function(movie) {
+        if (callBack) {					
+            callBack( {
+                'title':movie.Title,
+                'year':movie.Year,
+                'rated':movie.Rated,
+                'genre':movie.Genre,
+                'director':movie.Director,
+                'actors':movie.Actors,
+                'plot':movie.Plot,
+                'poster':movie.Poster
+            })
+        }            
+    })        
 }
 
 module.exports.searchAPI = searchAPI
-module.exports.addToArray = addToArray
